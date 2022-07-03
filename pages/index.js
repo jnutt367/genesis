623acc441d631c6803f2c44a952e6ed2d57cab60
Jason Nutt <jnutt367@gmail.com>
2022-07-03 18:02:40 -0400

    commit for index and 1st corinthians

--- pages/index.js
+++ pages/index.js
@@ -106,7 +106,7 @@ export default function Home() {
         Romans
         </li>
         </a>
-        <a href="#">
+        <a href="https://first-corinthians.vercel.app/">
         <li>
         1 Corinthians
         </li>
