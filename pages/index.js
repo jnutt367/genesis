c1b47ee7ecbe03976b3c47dadbdb21ece23b19b9
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-08-07 21:41:27 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1276,6 +1276,67 @@ Jacob’s Dream at Bethel
 20 Then Jacob made a vow, saying, “If God will be with me and will watch over me on this journey I am taking and will give me food to eat and clothes to wear 21 so that I return safely to my father’s household, then the Lord[f] will be my God 22 and[g] this stone that I have set up as a pillar will be God’s house, and of all that you give me I will give you a tenth.”
   </p>
           </div>
+
+    <div
+            className={styles.card}
+          >
+          <h2  className={styles.chapter}>GenesisChapter XXIX</h2>
+                                          <p>
+                                         
+                              Jacob Arrives in Paddan Aram
+29 Then Jacob continued on his journey and came to the land of the eastern peoples. 2 There he saw a well in the open country, with three flocks of sheep lying near it because the flocks were watered from that well. The stone over the mouth of the well was large. 3 When all the flocks were gathered there, the shepherds would roll the stone away from the well’s mouth and water the sheep. Then they would return the stone to its place over the mouth of the well.
+
+4 Jacob asked the shepherds, “My brothers, where are you from?”
+
+“We’re from Harran,” they replied.
+
+5 He said to them, “Do you know Laban, Nahor’s grandson?”
+
+“Yes, we know him,” they answered.
+
+6 Then Jacob asked them, “Is he well?”
+
+“Yes, he is,” they said, “and here comes his daughter Rachel with the sheep.”
+
+7 “Look,” he said, “the sun is still high; it is not time for the flocks to be gathered. Water the sheep and take them back to pasture.”
+
+8 “We can’t,” they replied, “until all the flocks are gathered and the stone has been rolled away from the mouth of the well. Then we will water the sheep.”
+
+9 While he was still talking with them, Rachel came with her father’s sheep, for she was a shepherd. 10 When Jacob saw Rachel daughter of his uncle Laban, and Laban’s sheep, he went over and rolled the stone away from the mouth of the well and watered his uncle’s sheep. 11 Then Jacob kissed Rachel and began to weep aloud. 12 He had told Rachel that he was a relative of her father and a son of Rebekah. So she ran and told her father.
+
+13 As soon as Laban heard the news about Jacob, his sister’s son, he hurried to meet him. He embraced him and kissed him and brought him to his home, and there Jacob told him all these things. 14 Then Laban said to him, “You are my own flesh and blood.”
+
+Jacob Marries Leah and Rachel
+After Jacob had stayed with him for a whole month, 15 Laban said to him, “Just because you are a relative of mine, should you work for me for nothing? Tell me what your wages should be.”
+
+16 Now Laban had two daughters; the name of the older was Leah, and the name of the younger was Rachel. 17 Leah had weak[a] eyes, but Rachel had a lovely figure and was beautiful. 18 Jacob was in love with Rachel and said, “I’ll work for you seven years in return for your younger daughter Rachel.”
+
+19 Laban said, “It’s better that I give her to you than to some other man. Stay here with me.” 20 So Jacob served seven years to get Rachel, but they seemed like only a few days to him because of his love for her.
+
+21 Then Jacob said to Laban, “Give me my wife. My time is completed, and I want to make love to her.”
+
+22 So Laban brought together all the people of the place and gave a feast. 23 But when evening came, he took his daughter Leah and brought her to Jacob, and Jacob made love to her. 24 And Laban gave his servant Zilpah to his daughter as her attendant.
+
+25 When morning came, there was Leah! So Jacob said to Laban, “What is this you have done to me? I served you for Rachel, didn’t I? Why have you deceived me?”
+
+26 Laban replied, “It is not our custom here to give the younger daughter in marriage before the older one. 27 Finish this daughter’s bridal week; then we will give you the younger one also, in return for another seven years of work.”
+
+28 And Jacob did so. He finished the week with Leah, and then Laban gave him his daughter Rachel to be his wife. 29 Laban gave his servant Bilhah to his daughter Rachel as her attendant. 30 Jacob made love to Rachel also, and his love for Rachel was greater than his love for Leah. And he worked for Laban another seven years.
+
+Jacob’s Children
+31 When the Lord saw that Leah was not loved, he enabled her to conceive, but Rachel remained childless. 32 Leah became pregnant and gave birth to a son. She named him Reuben,[b] for she said, “It is because the Lord has seen my misery. Surely my husband will love me now.”
+
+33 She conceived again, and when she gave birth to a son she said, “Because the Lord heard that I am not loved, he gave me this one too.” So she named him Simeon.[c]
+
+34 Again she conceived, and when she gave birth to a son she said, “Now at last my husband will become attached to me, because I have borne him three sons.” So he was named Levi.[d]
+
+35 She conceived again, and when she gave birth to a son she said, “This time I will praise the Lord.” So she named him Judah.[e] Then she stopped having children.
+
+
+  </p>
+          </div>
+
+  
             <div className={styles.iframe}>
               <iframe width="100%" height="315" src="https://www.youtube.com/embed/F4isSyennFo?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   
@@ -1586,20 +1647,7 @@ Aaron to Speak for Moses
           >
             <h2 className={styles.chapter2}>!Explaination of Layout!</h2>
             <p>
-            I have appended 1 Thessalonians to the bottom of 1 corinthians...
-            I have appended 2 Thessalonians to the bottom of 2 corinthians...
-            I have appended 1 Timothy to the bottom of the gospel of Matthew...
-            I will be appending 2 Timothy to the bottom of the gospel of Mark...
-            I will be appending Titus to the bottom of the gospel of Luke...
-            I will be appending Philemon to the bottom of the gospel of John...
-            I will be appending Hebrews to the bottom of the book of Acts ...
-            I will be appending James ...TBA
-            I will be appending Peter ...TBA
-            I will be appending 2 Peter ...TBA
-            I will be appending 1 John ...TBA
-            I will be appending 2 John ...TBA
-            I will be appending 3 John ...TBA
-            I will be appending Jude ...TBA
+            
             The New Testament IS COMPLETED!!!
             This is the beginning of the Old Testament and it will contain the 5 books of the Torah or Pentateuch...
               The meaning of “Torah” is often restricted to signify the first five books of the Hebrew Bible (Old Testament), also called the Law (or the Pentateuch, in Christianity). These are the books traditionally ascribed to Moses, the recipient of the original revelation from God on Mount Sinai.
