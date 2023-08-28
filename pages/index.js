a69e1bbe2e34e58f57777844b05cc180e44b8da2
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-08-27 20:45:25 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13485,7 +13485,7 @@ The meaning of “Torah” is often restricted to signify the first five books o
 
                         </div>
     <p className={styles.parables}>
-I Kings is coming
+I Kings records the history of Israel's divided kingdom & includes the building of the temple & palace in Jerusalem.
            
 </p>                                                      
                   <div className={styles.nav2}>           
@@ -14678,6 +14678,351 @@ The meaning of “Torah” is often restricted to signify the first five books o
 
 
 
+<main id="second_kings" className={styles.main}>
+       
+        <div className={styles.hero}>
+                <h2 className={styles.holy}>THE WORD</h2>
+                <h1 className={styles.bible}>
+                    <a href="https://the-word-of-god.vercel.app/" target="_blank">OF GOD</a>
+                </h1>
+        <h1 className={styles.title}>
+       II KINGS
+        </h1>
+          <div className={styles.image}>
+                           <Image 
+                                                    src="/kings.png"
+                                                    alt="hero image for Leviticus"
+                                                    height={900}
+                                                    width={2000}
+                                                    priority
+                                                  />
+                          
+
+                        </div>
+    <p className={styles.parables}>
+II Kings is in the process of being coded.
+           
+</p>                                                      
+                  <div className={styles.nav2}>           
+      <a href="https://mytorah.vercel.app/#numbers">
+             <Image 
+                                src="/previous.webp"
+                                alt="prev"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+       </a>
+
+        
+      <a href="https://cash.app/$JNutt367">
+              <Image className={styles.arrow}
+                                src="/donations_please.webp"
+                                alt="donations_please.webp"
+                                height={75}
+                                width={90}
+                                priority
+                              /> 
+       </a>
+         <a href="https://mytorah.vercel.app/#joshua">
+           <Image 
+                                src="/next.webp"
+                                alt="next"
+                                 height={75}
+                                width={90}
+                                priority
+                              />
+           
+        </a> 
+         
+               </div>
+                                 <div className={styles.iframe}> 
+                                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/bVFW3wbi9pk?si=3z0utcFzJ3Il-OXY?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                  </div>
+                              
+        <div className={styles.grid2}>
+          
+
+   <div className={styles.card6}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter I </h2>
+                                <p>
+ <h4 className={styles.h4}>                           </h4> 
+
+
+
+
+                                  
+                                   </p>
+  </div>
+
+   <div className={styles.card4}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter II </h2>
+                                <p>
+ <h4 className={styles.h4}>                         </h4> 
+
+
+
+                                            </p>
+                                  </div>
+
+<div className={styles.card6}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter III </h2>
+                                <p>
+ <h4 className={styles.h4}>                      </h4> 
+
+
+                               </p>
+                                  </div>
+
+
+ <div className={styles.card4}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter IV </h2>
+                                <p>
+ <h4 className={styles.h4}>                      </h4> 
+
+                               </p>
+                                  </div>
+
+<div className={styles.card6}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter V </h2>
+                                <p>
+ <h4 className={styles.h4}>                   </h4> 
+
+              </p>
+                                  </div>
+
+
+  <div className={styles.card4}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter VI </h2>
+                                <p>
+ <h4 className={styles.h4}>                       </h4> 
+           
+  
+                                     </p>
+                                  </div>
+
+<div className={styles.card6}
+          >
+            <h2 className={styles.chapter3}>II Knigs Chapter VII </h2>
+                                <p>
+ <h4 className={styles.h4}>                      </h4> 
+
+
+
+
+                                  </p>
+                                  </div>  
+
+  
+<div className={styles.card4}
+          >
+            <h2 className={styles.chapter3}>II Knigs Chapter VIII </h2>
+                                <p>
+ <h4 className={styles.h4}>                      </h4> 
+
+                               </p>
+                                  </div>
+
+
+
+  <div className={styles.card6}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter IX </h2>
+                                <p>
+ <h4 className={styles.h4}>                  </h4> 
+    
+                                </p>
+                
+  </div>  
+
+
+<div className={styles.card4}
+          >
+            <h2 className={styles.chapter3}>I Kings Chapter X </h2>
+                                <p>
+ <h4 className={styles.h4}>                    </h4> 
+
+
+ 
+                                 </p>
+                                  </div>
+
+<div className={styles.card6}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter XI </h2>
+                                <p>
+ <h4 className={styles.h4}>               </h4> 
+                
+
+
+                                </p>
+                                  </div>
+
+
+<div className={styles.card4}
+          >
+            <h2 className={styles.chapter3}>II Knigs Chapter XII</h2>
+                                <p>
+ <h4 className={styles.h4}>                 </h4> 
+
+                          
+                                    </p>
+                                  </div>
+
+
+
+  <div className={styles.card6}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter XIII </h2>
+                                <p>
+ <h4 className={styles.h4}>                      </h4> 
+
+  
+                                </p>
+                                  </div>
+
+<div className={styles.card4}
+          >
+            <h2 className={styles.chapter3}>I Kings Chapter XIV </h2>
+                                <p>
+ <h4 className={styles.h4}>                  </h4> 
+
+
+                              </p>
+                                  </div>
+
+
+  
+  <div className={styles.card6}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter XV </h2>
+                                <p>
+ <h4 className={styles.h4}>                 </h4> 
+
+ 
+
+  
+                                                </p>
+                                  </div>
+
+
+  <div className={styles.card4}
+          >
+            <h2 className={styles.chapter3}>II Knigs Chapter XVI </h2>
+                                <p>
+ <h4 className={styles.h4}>                      </h4> 
+
+                                 </p>
+                                  </div>
+
+<div className={styles.card6}
+          >
+            <h2 className={styles.chapter3}>I Kings Chapter XVII </h2>
+                                <p>
+                     <h4 className={styles.h4}>                    </h4>   
+                                </p>
+                                  </div>
+
+<div className={styles.card4}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter XVIII </h2>
+                                <p>
+ <h4 className={styles.h4}>                    </h4> 
+
+            
+                                  </p>
+                                  </div>
+
+<div className={styles.card6}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter XIX </h2>
+                                <p>
+ <h4 className={styles.h4}>                  </h4> 
+
+                                </p>
+  </div>
+
+<div className={styles.card6}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter XXI</h2>
+                                <p>
+ <h4 className={styles.h4}>                 </h4> 
+
+
+                                   </p>
+                                  </div>
+  
+
+<div className={styles.card4}
+          >
+            <h2 className={styles.chapter3}>II Kings Chapter XXII </h2>
+                                <p>
+ <h4 className={styles.h4}>                    </h4> 
+
+
+                      </p>
+                                  </div>
+
+
+
+  
+  
+          <div className={styles.card4}
+          >
+            <h2 className={styles.chapter3}>!Explaination of Layout!</h2>
+            <p>
+            
+            <h4 className={styles.h4}> New Testament IS COMPLETED!!!</h4>
+            This is the beginning of the Old Testament and it will contain the 5 books of the Torah or Pentateuch...
+              <h4 className={styles.h4}>Genesis Completed</h4>
+               <h4 className={styles.h4}>Exodus is Completed</h4>
+              <h4 className={styles.h4}>Leviticus is Completed</h4>
+               <h4 className={styles.h4}>Numbers is Completed</h4>
+               <h4 className={styles.h4}> Deuteronomy is Completed</h4>
+              <h4 className={styles.h4}>This concludes the Torah (Pentateuch) </h4>
+
+The meaning of “Torah” is often restricted to signify the first five books of the Hebrew Bible (Old Testament), also called the Law (or the Pentateuch, 
+              in Christianity). 
+              These are the books traditionally ascribed to Moses, the recipient of the original revelation from God on Mount Sinai.
+              More to come on the layout...!
+              
+             <h4 className={styles.h4}>Next is the Neviʾim (Prophets)</h4>
+              <h4 className={styles.h4}>Joshua is Completed </h4>  
+              <h4 className={styles.h4}>Judges is Completed </h4>    
+              <h4 className={styles.h4}>Ruth is Completed </h4>    
+              <h4 className={styles.h4}>I Samuel is Completed</h4>   
+             <h4 className={styles.h4}>II Samuel is Completed</h4>    
+            <h4 className={styles.h4}>I Kings is Completed</h4>   
+            <h4 className={styles.h4}>II Kings is Completed</h4>                       
+                </p>
+          </div>
+
+
+     
+
+        </div>
+</div>
+        <p className={styles.description}> 
+        <code className={styles.publisher}>
+        Coded, Published & Deployed by His servant: Jason Daniel Nutt 
+        </code>
+        </p>
+      </main>  
+
+
+
+
+
+
+
+
+  
 
 
                 
